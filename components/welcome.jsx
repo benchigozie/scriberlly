afe1afba98640d752b03b739eee43dca4a0c7225
Ben Chigozie <115552805+benchigozie@users.noreply.github.com.>
2025-04-12 22:50:10 +0100

    added and implemented the typing effect on the welcome component.

--- components/welcome.jsx
+++ components/welcome.jsx
@@ -1,9 +1,28 @@
-import React from 'react'
+import React, { useEffect, useRef } from 'react';
+import Typed from 'typed.js';
 
 function Welcome() {
+
+    const typedRef = useRef(null);
+
+    useEffect(() => {
+        const typed = new Typed(typedRef.current, {
+          strings: ['Scriberlly'],
+          typeSpeed: 70,
+          backSpeed: 40,
+          startDelay: 500,
+          backDelay: 5000,
+          loop: true,
+          showCursor: true,
+          cursorChar: '.',
+        });
+    
+        return () => typed.destroy(); 
+      }, []);
+
   return (
-    <div className='bg-red-600 text-3xl font-marker'>
-        <p>Scriberlly</p>
+    <div className='bg-my-off-white text-3xl font-marker flex items-center justify-center py-16'>
+        <span ref={typedRef} >Scriberlly</span>
     </div>
   )
 }
